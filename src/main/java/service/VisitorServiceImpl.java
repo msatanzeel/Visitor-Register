5841c812f02e96f152caae53c82e1c6ed3536aad
MohammadSajidAliTanzeel <mdtanzeel.sajid@sprinklr.com>
2024-01-23 22:59:34 +0530

    Fixed entity visitor

--- src/main/java/service/VisitorServiceImpl.java
+++ src/main/java/service/VisitorServiceImpl.java
@@ -1,48 +1,5 @@
 package service;
 
-import javax.persistence.*;
-import java.util.Date;
-
-enum VisitorType {
-    EMPLOYEE, CONTRACTOR, CLIENT, OTHER
-}
-
-@Entity
-@Table(name = "visitor_table")
 public class VisitorServiceImpl implements VisitorService{
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "visitor_id")
-    private int visitorID;
 
-    @Column(name = "name")
-    private String name;
-
-    @Column(name = "email")
-    private String email;
-
-    @Column(name = "phone")
-    private String phone;
-
-    @Column(name = "checkin_time")
-    private Date checkinTime;
-
-    @Column(name = "checkout_time")
-    private Date checkoutTime;
-
-    @Column(name = "duration")
-    private String duration;
-
-    @Column(name = "purpose_of_visit")
-    private String purposeOfVisit;
-
-    @Column(name = "company_or_organization")
-    private String companyOrOrganization;
-
-    @Column(name = "vehicle_number")
-    private String vehicleNumber;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "visitor_type")
-    private VisitorType visitorType;
 }
