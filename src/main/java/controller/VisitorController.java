c4d12ad2b71d7bd6bfd2d31425b252aa81df8635
MohammadSajidAliTanzeel <mdtanzeel.sajid@sprinklr.com>
2024-02-03 16:56:18 +0530

    Sending the response as JSON using ResponseEntity

--- src/main/java/controller/VisitorController.java
+++ src/main/java/controller/VisitorController.java
@@ -1,29 +1,31 @@
 package controller;
 
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.HttpMediaTypeException;
 import org.springframework.web.bind.annotation.*;
 import repository.entities.Visitor;
 
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @Controller
 public class VisitorController {
 
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
-        return ResponseEntity.badRequest().body("Invalid request body");
+    @RequestMapping(value = "/addVisitor", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> addVisitor(Model model, @RequestBody Visitor visitor) {
+        System.out.println("The visitor information is as follows: " + visitor);
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "success");
+        response.put("message", "Visitor information has been added successfully");
+        response.put("data", visitor);
+        return ResponseEntity.ok(response);
     }
 
-    @RequestMapping(value = "/addVisitor", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public String addVisitor(Model model, @RequestBody Visitor visitor) {
-        System.out.println("The visitor information is as follows: " + visitor);
-        return "addVisitor";
+    public String updateVisitor(Model model, @RequestBody Visitor visitor) {
+        return "";
     }
 
 }
